a69e9a946037dd9fadbbf9a1540ce3d23f36211b
Adi <adibakija35@hotmail.com>
2018-12-17 03:50:17 +0000

    Updated app component to be stateful and setup basic pet adoption page layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_Key,
+  secret: process.env.API_Secret
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Syracuse, NY" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet))
+            pets = data.petfinder.pets.pet;
+          else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
+  }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Rocky" animal="Parrot" breed="Green cheeked conure" />
-        <Pet name="Kiki" animal="Parrot" breed="Budgie" />
-        <Pet name="Riki" animal="Parrot" breed="Cockatiel" />
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
